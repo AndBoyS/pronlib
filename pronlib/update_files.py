0a4f1f9f8b36815cf5fed55ab846c8f7bd35b0bd
AndBoyS <andboyxd@gmail.com>
2022-08-23 22:01:24 +0300

    subfolders auto index completion

--- pronlib/update_files.py
+++ pronlib/update_files.py
@@ -19,33 +19,37 @@ class FileUpdater(ABC):
         self.main_folders = self.get_files(base_dir)
         self.start_index = start_index
     
-    def update(self, folders=None, start_index=None, update_total=True):
+    def update(self, folders=None, start_index=None, recursive_update=True):
         
         if folders is None:
             folders = self.main_folders
+
         if start_index is None:
             start_index = self.start_index
-        cur_id = start_index
         
         temp_folders = []
             
-        for folder in folders:
+        for cur_id, folder in enumerate(folders, start=start_index):
             name_components = folder.name.split()
-        
+
+            # Если у папки нет индекса
+            if re.match(r'\d+$', name_components[0]) is None:
+                name_components.insert(0, str(cur_id))
+
+            # Изменение индекса
             if name_components[0] != str(cur_id):
                 name_components[0] = str(cur_id)
-                
-            if update_total:
+
+            # Обновление подпапок
+            if recursive_update:
                 child_files = self.get_files(folder)
                 self.update_child_files(child_files)
                 self.change_total(name_components, child_files)
-                
+
             new_name = ' '.join(name_components)
             # Сначала переименовываем в "новое имя_", потом в "новое имя"
             # чтобы избежать конфликтов, когда файл "новое имя" уже есть
             temp_folders.append(folder.rename(folder.parent / (new_name+'_')))
-    
-            cur_id += 1
 
         for folder in temp_folders:
             folder.rename(folder.parent / folder.name[:-1])
@@ -64,17 +68,20 @@ class FileUpdater(ABC):
         else:
             name_components.append(new_total_str)
             
-    def get_files(self, base_dir=None):
+    def get_files(self, base_dir=None, childs_are_folders=None):
         '''
         Получить список файлов в директории
         '''
         if base_dir is None:
             base_dir = self.base_dir
 
+        if childs_are_folders is None:
+            childs_are_folders = self.childs_are_folders
+
         # Файлы являются папками
-        if self.childs_are_folders:
+        if childs_are_folders:
             folders = [path for path in base_dir.iterdir() 
-                       if path.is_dir() and path.name[0].isdigit()]
+                       if path.is_dir()]
             return natsorted(folders, key=lambda x: x.name)
         # Файлы не являются папками
         else:
@@ -97,7 +104,7 @@ class PhotoUpdater(FileUpdater):
     def update_child_files(self, child_files):
         
         # Для директории с фото обновляются также подпапки
-        self.update(child_files, start_index=1, update_total=False)
+        self.update(child_files, start_index=1, recursive_update=False)
         
         
 class VideoUpdater(FileUpdater):
@@ -105,7 +112,7 @@ class VideoUpdater(FileUpdater):
     childs_are_folders = False
     
     def __init__(self, base_dir, photos_dir):
-        start_index = len(self.get_files(photos_dir))
+        start_index = len(self.get_files(photos_dir, childs_are_folders=True))
         super().__init__(base_dir, start_index=start_index)
 
     def update_child_files(self, child_files):
