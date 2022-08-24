050776ed8b688d9e863f1d1882c9d71607eae78a
AndBoyS <andboyxd@gmail.com>
2022-08-24 12:46:07 +0300

    artist name extraction from folder name

--- pronlib/update_meta.py
+++ pronlib/update_meta.py
@@ -12,8 +12,7 @@ subfolder_name_pattern = re.compile(r'\d+ (.+)')
 def get_subfolders(folder: Path):
     return natsorted([path for path in folder.glob('*') if path.is_dir()], 
                      key=lambda x: x.name)
-                  
-                  
+
 
 def update_meta_files(base_folder: Path, summary_path='summary.txt'):
     '''
@@ -48,6 +47,16 @@ def update_meta_files(base_folder: Path, summary_path='summary.txt'):
         f.write(summary)
 
 
+def load_json(fp: Path):
+    with open(fp, 'r') as f:
+        return json.load(f)
+
+
+def dump_json(obj, fp: Path):
+    with open(fp, 'w') as f:
+        json.dump(obj, f)
+
+
 def get_set_meta_data(folder: Path, re_pattern) -> (str, str):
     '''
     Получить мета данные из folder / 'meta.json' + создать meta.json если он отсутствует
@@ -57,18 +66,43 @@ def get_set_meta_data(folder: Path, re_pattern) -> (str, str):
     if folder_name_match is None:
         return '', ''
     folder_name = folder_name_match.group(1)
+
+    # Если файла meta.json нет в подпапке
     if not list(folder.glob('meta.json')):
-        with open(folder / 'meta.json', 'w') as f:
-            meta_data = {'name': folder_name,
-                         'artist': ''}
-            json.dump(meta_data, f)
 
-        return folder_name, ''
+        meta_artist = extract_artist_from_folder_name(folder_name)
 
+        meta_data = {'name': folder_name,
+                     'artist': meta_artist}
+
+        dump_json(meta_data, folder / 'meta.json')
+        meta_name = folder_name
+
+    # Если файл meta.json существует
     else:
-        with open(folder / 'meta.json', 'r') as f:
-            meta_data = json.load(f)
-            return meta_data['name'], meta_data['artist']
+        meta_data = load_json(folder / 'meta.json')
+
+        # Если не указан художник, попробовать чекнуть название папки
+        # и обновить метадату
+        if not meta_data['artist']:
+            meta_data['artist'] = extract_artist_from_folder_name(folder_name)
+            if meta_data['artist']:
+                dump_json(meta_data, folder / 'meta.json')
+
+        meta_name = meta_data['name']
+        meta_artist = meta_data['artist']
+
+    return meta_name, meta_artist
+
+
+def extract_artist_from_folder_name(folder_name: str):
+    # Имя автора может находится в названии папки
+    # пример: big booba doujinshi [artist name]
+    meta_artist_match = re.search(r'\[(.+)\]', folder_name)
+    if meta_artist_match is None:
+        return ''
+    else:
+        return meta_artist_match.group(1)
 
 
 def update_random_file(photos_dir, videos_dir, file_path='sauce.json'):
