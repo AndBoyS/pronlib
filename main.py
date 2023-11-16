41c064e8537ffea1e3a17e340ddbd375452fa8c2
AndBoyS <andboyxd@gmail.com>
2023-11-16 21:21:24 +0300

    oop index rework

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from pathlib import Path
+import subprocess
 
-from pronlib.folder_index import FolderIndexer, get_subfolders
+from natsort import natsorted
+
+from pronlib.folder_index import MediaChapter, PhotoChapter, VideoChapter, get_subfolders, reindex_folders, save_sauce
 
 
 def main() -> None:
-    root_path = Path(__file__).parents[1] / "Test"
+    repo_dir = Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip())
+    media_dir = repo_dir.parent / "Test"
 
-    folder_indexer = FolderIndexer()
+    sauce_save_path = repo_dir / "sauce.json"
 
-    video_path = root_path / "Videos"
-    photo_path = root_path / "Photos"
+    video_path = media_dir / "Videos"
+    photo_path = media_dir / "Photos"
 
-    num_video_folders = len(get_subfolders(video_path))
-
-    folder_indexer.reindex_video_folders(
-        video_path,
-    )
-
-    folder_indexer.reindex_photo_folders(
-        photo_path,
-        start_index=num_video_folders + 1,
-    )
-    folder_indexer.save_sauce()
+    video_folders = natsorted(get_subfolders(video_path))
+    num_videos = len(video_folders)
+    photo_folders = natsorted(get_subfolders(photo_path))
     
+    video_chapters: list[MediaChapter] = [VideoChapter(path, i) for i, path in enumerate(video_folders, start=1)]
+    photo_chapters: list[MediaChapter] = [
+        PhotoChapter(path, i) for i, path in enumerate(photo_folders, start=num_videos + 1)
+    ]
 
-if __name__ == '__main__':
+    reindex_folders(video_chapters)
+    reindex_folders(photo_chapters)
+    
+    save_sauce(
+        video_chapters + photo_chapters,
+        save_path=sauce_save_path,
+    )
+
+
+if __name__ == "__main__":
     main()
--- pronlib/folder_index.py
+++ pronlib/folder_index.py
@@ -1,41 +1,168 @@
 from abc import ABC, abstractmethod
+import json
 from pathlib import Path
-from typing import Type
+import re
+
+from natsort import natsorted
 
 
 def get_subfolders(path: Path) -> list[Path]:
     return [p for p in path.iterdir() if p.is_dir() and not p.name.startswith(".")]
 
 
-class MediaChapter(ABC):
-    @abstractmethod
-    def __init__(self, chapter_path: Path) -> None:
-        pass
+def get_content_subfiles(path: Path) -> list[Path]:
+    return [p for p in path.iterdir() if p.name.lower() != "meta.json" and not p.name.startswith(".")]
+
+
+class Media(ABC):
+    path: Path
+    title: str
+    index: int
     
     @abstractmethod
-    def set_index(self, i: int) -> None:
+    def __init__(self, chapter_path: Path, index: int) -> None:
         pass
-    
+
     @abstractmethod
     def rename_to_temp(self) -> None:
         pass
     
+    @property
     @abstractmethod
+    def full_title(self) -> str:
+        pass
+    
     def rename_update(self) -> None:
-        pass
-    
-    
+        new_path = self.path.with_name(f'{self.full_title}{self.path.suffix}')
+        self.path = self.path.rename(new_path)
 
-class FolderIndexer:
-    def reindex_folders(self, media_path: Path, chapter_type: Type[MediaChapter], start_index: int = 1) -> None:
-        chapters = [chapter_type(path) for path in get_subfolders(media_path)]
+
+
+class Video(Media):
+    def __init__(self, path: Path, index: int) -> None:
+        self.path = path
+        self.index = index
+        self.title = self.path.stem.replace("_temp", "")
+        self.title = re.sub(r"^\d+", "", self.title).strip()
+
+    @property
+    def full_title(self) -> str:
+        return f"{self.index} {self.title}".strip()
+
+    def rename_to_temp(self) -> None:
+        temp_name = f"{self.full_title}_temp{self.path.suffix}"
+        temp_path = self.path.with_name(temp_name)
+        self.path = self.path.rename(temp_path)
+
+
+class PhotoFolder(Media):
+    def __init__(self, path: Path, index: int) -> None:
+        self.path = path
+        self.index = index
+        self.title = self.path.stem.replace("_temp", "")
+        self.title = re.sub(r"^\d+", "", self.title)
         
-        for i, chapter in enumerate(chapters, start=start_index):
-            chapter.set_index(i)
-            chapter.rename_to_temp()
-            
-        for chapter in chapters:
-            chapter.rename_update()
+        if not self.path.is_dir():
+            raise ValueError(f"{self.path} is not a dir, but its expected to be")
 
-    def save_sauce(self) -> None:
+        self.artist_name = None
+        artist_ptrn = re.compile(r"\[(.+)\]$")
+        artist_match = artist_ptrn.search(self.title)
+        if artist_match is not None:
+            self.artist_name = artist_match.group(1).title()
+
+        self.title = artist_ptrn.sub("", self.title).strip()
+
+    @property
+    def full_title(self) -> str:
+        full_title = f"{self.index} {self.title}".strip()
+        if self.artist_name is not None:
+            full_title = f"{full_title} [{self.artist_name}]"
+
+        return full_title
+    
+    def rename_to_temp(self) -> None:
+        temp_path = self.path.with_name(f"{self.path.name}_temp")
+        self.path = self.path.rename(temp_path)
+
+    def rename_update(self) -> None:
+        new_path = self.path.with_name(self.full_title)
+        self.path = self.path.rename(new_path)
+
+
+class MediaChapter(ABC):
+    path: Path
+    title: str
+    index: int
+    count: int
+    media_list: list[Media]
+
+    @abstractmethod
+    def __init__(self, chapter_path: Path, index: int) -> None:
         pass
+
+    def rename_to_temp(self) -> None:
+        temp_path = self.path.with_name(f"{self.full_title}_temp")
+        self.path = self.path.rename(temp_path)
+
+    def rename_update(self) -> None:
+        new_path = self.path.with_name(self.full_title)
+        self.path = self.path.rename(new_path)
+
+    @property
+    def full_title(self) -> str:
+        return f"{self.index} {self.title} ({self.count})".strip()
+
+
+class VideoChapter(MediaChapter):
+    def __init__(self, chapter_path: Path, index: int) -> None:
+        self.path = chapter_path
+        self.index = index
+
+        if not self.path.is_dir():
+            raise ValueError(f"{self.path} is not a dir, but its expected to be")
+
+        self.title = self.path.stem.replace("_temp", "")
+        self.title = re.sub(r"^\d+", "", self.title)
+        self.title = re.sub(r"\(\d+\)$", "", self.title).strip()
+
+        media_paths = natsorted(get_content_subfiles(self.path))
+        self.media_list = [Video(p, i) for i, p in enumerate(media_paths, start=1)]
+        self.count = len(self.media_list)
+
+
+class PhotoChapter(MediaChapter):
+    def __init__(self, chapter_path: Path, index: int) -> None:
+        self.path = chapter_path
+        self.index = index
+
+        if not self.path.is_dir():
+            raise ValueError(f"{self.path} is not a dir, but its expected to be")
+
+        self.title = self.path.stem.replace("_temp", "")
+        self.title = re.sub(r"^\d+", "", self.title)
+        self.title = re.sub(r"\(\d+\)$", "", self.title).strip()
+
+        media_paths = natsorted(get_content_subfiles(self.path))
+        self.media_list = [PhotoFolder(p, i) for i, p in enumerate(media_paths, start=1)]
+        self.count = len(self.media_list)
+
+
+def reindex_folders(chapters: list[MediaChapter]) -> None:
+    
+    for chapter in chapters:
+        for media in chapter.media_list:
+            media.rename_to_temp()
+        for media in chapter.media_list:
+            media.rename_update()
+
+    for chapter in chapters:
+        chapter.rename_to_temp()
+    for chapter in chapters:
+        chapter.rename_update()
+
+
+def save_sauce(chapters: list[MediaChapter], save_path: Path | str) -> None:
+    sauce = {f"{chapter.index} {chapter.title}": chapter.count for chapter in chapters}
+    with open(save_path, "w", encoding="utf8") as f:
+        json.dump(sauce, f)
