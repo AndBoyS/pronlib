ce30e93b51c05d12fdb160ca74b662b2323db928
AndBoyS <andboyxd@gmail.com>
2023-11-20 20:48:45 +0300

    debug constant

--- pronlib/constants.py
+++ pronlib/constants.py
@@ -2,8 +2,15 @@ from pathlib import Path
 import subprocess
 
 
+DEBUG = True
 REPO_PATH = Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip())
-VIDEO_PATH = REPO_PATH.parent / "[Content]/Videos"
-PHOTO_PATH = REPO_PATH.parent / "[Content]/Photos"
+
+if DEBUG:
+    MEDIA_PATH = REPO_PATH / 'tests/test_media'
+else:
+    MEDIA_PATH = REPO_PATH.parent / '[Content]'
+
+VIDEO_PATH = MEDIA_PATH / "Videos"
+PHOTO_PATH = MEDIA_PATH / "Photos"
 SAUCE_PATH = REPO_PATH / "sauce.json"
 META_NAME = "meta.json"
